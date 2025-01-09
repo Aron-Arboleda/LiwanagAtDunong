47151228eb84873ea37baf8777d18b526375f691
Aron-Arboleda <aronrez62@gmail.com>
2025-01-10 01:30:18 +0800

    Removed the .JPG bugs

--- src/assets/images/PageImages/FAQsPage/index.js
+++ src/assets/images/PageImages/FAQsPage/index.js
@@ -1,5 +1,5 @@
 import VolunteersTravelingByFoot from "./VolunteersTravelingByFoot.jpg";
-import VolunteerTeaching from "./VolunteerTeaching.jpg";
+import VolunteerTeaching from "./VolunteerTeaching.png";
 import VolunteerHavingFunWithAetaKids from "./VolunteerHavingFunWithAetaKids.jpg";
 
 export {
