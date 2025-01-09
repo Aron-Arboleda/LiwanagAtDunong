e3dc269a152ea4d9ff58685109189d1a99fc0bf7
Aron-Arboleda <aronrez62@gmail.com>
2025-01-10 01:27:58 +0800

    Removed viteImagemin for now

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import viteImagemin from "vite-plugin-imagemin";
+// import viteImagemin from "vite-plugin-imagemin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 75,
-      },
-      pngquant: {
-        quality: [0.65, 0.8],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          { name: "removeViewBox" },
-          { name: "removeEmptyAttrs", active: false },
-        ],
-      },
-      webp: {
-        quality: 75,
-      },
-      avif: {
-        quality: 50,
-      },
-    }),
+    // viteImagemin({
+    //   gifsicle: {
+    //     optimizationLevel: 7,
+    //     interlaced: false,
+    //   },
+    //   optipng: {
+    //     optimizationLevel: 7,
+    //   },
+    //   mozjpeg: {
+    //     quality: 75,
+    //   },
+    //   pngquant: {
+    //     quality: [0.65, 0.8],
+    //     speed: 4,
+    //   },
+    //   svgo: {
+    //     plugins: [
+    //       { name: "removeViewBox" },
+    //       { name: "removeEmptyAttrs", active: false },
+    //     ],
+    //   },
+    //   webp: {
+    //     quality: 75,
+    //   },
+    //   avif: {
+    //     quality: 50,
+    //   },
+    // }),
   ],
   resolve: {
     alias: {
