c5fb5334a81df4904fc44ea777c59ed3ae7cedb2
Aron-Arboleda <aronrez62@gmail.com>
2025-01-11 18:10:26 +0800

    Sidebar responsive

--- src/pages/adminPages/components/Sidebar/Sidebar.jsx
+++ src/pages/adminPages/components/Sidebar/Sidebar.jsx
@@ -1,51 +1,86 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Sidebar.css";
 import { LDLogo } from "@images/Graphics/WEBSITE_Graphics/IconsAndVectors";
-import { FileSpreadsheet, House } from "lucide-react";
+import { FileSpreadsheet, House, LayoutDashboard, Menu } from "lucide-react";
 
 const Sidebar = () => {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const sidebarRef = useRef(null);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   useEffect(() => {
     const handleLocationChange = () => {
       setCurrentPath(window.location.pathname);
     };
-
     window.addEventListener("popstate", handleLocationChange); // Update on browser back/forward.
+
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setIsMenuOpen(false); // Close sidebar if clicked outside
+        console.log("Clicked outside");
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
       window.removeEventListener("popstate", handleLocationChange);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, []);
+  }, [setIsMenuOpen]);
 
   const isActive = (path) => currentPath === path;
 
   return (
-    <div className="sidebar">
-      <div className="sidebarContainer">
-        <div className="flex-center">
-          <img
-            src={LDLogo}
-            alt="Liwanag at Dunong Logo"
-            className="sidebarLogo"
-          />
-        </div>
-        <ul className="sidebarList">
-          <li className={isActive("/admin") ? "active" : ""}>
-            <a href="/admin" className="navLinks">
-              <House size={24} className="icon" />
-              Home
-            </a>
-          </li>
-          <li className={isActive("/admin/submissions") ? "active" : ""}>
-            <a href="/admin/submissions" className="navLinks">
-              <FileSpreadsheet size={24} className="icon" />
-              Submissions
-            </a>
-          </li>
-        </ul>
+    <>
+      <div className="sidebarHeader">
+        <a href="/admin">
+          <LayoutDashboard size={24} className="icon" />
+          <h1 className="sidebarTitle">Dashboard</h1>
+        </a>
+        <button className="sidebarMenuButton" onClick={() => toggleMenu()}>
+          <Menu size={24} className="icon" />
+        </button>
       </div>
-    </div>
+      <div
+        ref={sidebarRef}
+        className={`sidebar ${isMenuOpen ? "open" : "closed"}`}
+      >
+        {/* <div>
+          <input type="checkbox" id="sideBarCheckbox" />
+          <label htmlFor="sideBarCheckbox" className="sidebarMenuButton">
+            <Menu size={24} className="icon" />
+          </label>
+        </div> */}
+        <div className="sidebarContainer">
+          <div className="flex-center">
+            <img
+              src={LDLogo}
+              alt="Liwanag at Dunong Logo"
+              className="sidebarLogo"
+            />
+          </div>
+          <ul className="sidebarList">
+            <li className={isActive("/admin") ? "active" : ""}>
+              <a href="/admin" className="navLinks">
+                <House size={20} className="icon" />
+                <span>Home</span>
+              </a>
+            </li>
+            <li className={isActive("/admin/submissions") ? "active" : ""}>
+              <a href="/admin/submissions" className="navLinks">
+                <FileSpreadsheet size={20} className="icon" />
+                <span>Submissions</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={`sidebarOverlay ${isMenuOpen ? "open" : "closed"}`}></div>
+    </>
   );
 };
 
