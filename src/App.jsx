932110d2e55542d68a2bc0a3c68d5161c0c5eefc
Aron-Arboleda <aronrez62@gmail.com>
2025-02-01 11:04:59 +0800

    Trying to fix the meta tags issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@ import "./styles/App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./Routes";
 import { AuthProvider } from "@contexts/AuthContext";
-import { HelmetProvider } from "react-helmet-async";
-import GlobalMetaTags from "@components/GlobalMetaTags/GlobalMetaTags";
 
 function App() {
   return (
-    <HelmetProvider>
-      <GlobalMetaTags />
-      <Router>
-        <AuthProvider>
-          <AppRoutes />
-        </AuthProvider>
-      </Router>
-    </HelmetProvider>
+    <Router>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </Router>
   );
 }
 
